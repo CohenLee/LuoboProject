8f60817986746a7c28a8e1fca8ad158a8a09ac3c
CohenLee <cohenlee@163.com>
2017-05-19 15:48:01 +0800

    的
    但是

---
+++ Assets/Game/Scripts/Application/Data/Tile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//格子信息
+public class Tile{
+
+    public int X;
+    public int Y;
+    public bool CanHold;   //这个格子是否可以放塔
+    public object Data;
+
+    public Tile(int x,int y) 
+    {
+        this.X = x;
+        this.Y = y;
+    }
+}
